496470f3eff9c9a2ebfe6190be0b25715da80c64
Beast2502 <mehulsaxena45@gmail.com>
2022-08-15 06:00:16 +0530

    initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,16 @@
+import CurrentLocation from "./CurrentLocation";
+const currentLoc = new CurrentLocation();
+
+
+const initApp =()=>{
+    // add listeners
+    const getButton = document.getElementById("getLocation");
+    geoButton.addEventListener("click",getGeoWeather);
+
+    //  set up
+
+
+    // load weather
+}
+
+document.addEventListener("DOMContentLoaded",initApp)
